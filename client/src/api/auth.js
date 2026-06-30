651d8eb53761d961397e94c63206078a8d686cfb
shubhs06 <shamacharekar48@gmail.com>
2026-06-30 18:31:18 +0530

    all done

--- client/src/api/auth.js
+++
@@ -1,5 +0,0 @@
-import api from './axios';
-
-export const registerUser = (data) => api.post('/auth/register', data);
-export const verifyOtp = (data) => api.post('/auth/verify-otp', data);
-export const loginUser = (data) => api.post('/auth/login', data);
